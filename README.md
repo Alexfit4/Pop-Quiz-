97d2605b181f6da9fe823d54feabf1d671042e77
Alex Ashtiany <alex.ashtiany@gmail.com>
2020-12-03 14:19:31 -0500

    tried to mess with JQuery no luck

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Pop-Quiz-
+
+## Inscructions:
+
+1. Design a timed quiz website.
+2. When user clicks btuton to start the quiz the countdown begins.
+3. After the user clicks on "Click here to start" a new page will be shown with a multiple choice question for the user to answer.
+4. After the user clicks on "Sumbit Answer" the user will then be notified if the users answer was correct or incorrect. If the user was correct then a point is added to their overall quiz score. If not the user will lose 5 seconds off the remaining time they have left.
+5. Once the user finishes the quiz or if all the time ran out, the user will be shown a screen to input there first name and their name will be logged and will be available to see if they click on the "View High Score" on the top right of the page, where they can see who else has taken the quiz and there final score.
--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let timeEl = document.querySelector(".time");
 var buttonEl = document.getElementById("button");
 let secondsLeft = 61;
+let correctAnswer = document.querySelector("#correct");
+let scoreEl = document.getElementById("score");
+let scoreLeft = 0;
+
+let theCorrectAnswer = (correctAnswer.onclick = function () {
+	correctAnswer.style.color = "green";
+	// scoreLeft++;
+	// scoreEl.textContent = "Score: " + scoreLeft;
+});
 
 buttonEl.addEventListener("click", function () {
 	let timerInterval = setInterval(function () {
@@ -15,24 +24,113 @@ buttonEl.addEventListener("click", function () {
 	document.querySelector(".section1").setAttribute("class", "hide");
 
 	document.querySelector("#section2").setAttribute("class", "show");
-
-	// function sendMessage() {
-	// 	timeEl.textContent = " ";
-
-	// 	let imgEl = document.createElement("img");
-
-	// 	imgEl.setAttribute("src", "images/image_1.jpg");
-	// 	mainEl.appendChild(imgEl);
-	// }
 });
 
 //----Section 2 --- //
 
 let secTwo = document.querySelector("#section2");
 
-document.querySelector("#section2").addEventListener("click", function () {
-	//------- Hide DIV----------//
+let submitTwo = document
+	.querySelector("#button2")
+	.addEventListener("click", function () {
+		
+			scoreLeft++;
+			scoreEl.textContent = "Score: " + scoreLeft;
+		
+		//------- Hide DIV----------//
 
-	secTwo.setAttribute("class", "hide");
-	document.querySelector("#section3").setAttribute("class", "show");
-});
+		secTwo.setAttribute("class", "hide");
+		document.querySelector("#section3").setAttribute("class", "show");
+	});
+console.log(submitTwo);
+console.log(theCorrectAnswer);
+
+// ----------------------------------------///
+//--- TESTING AREA ---///
+
+// console.log(questions);
+
+// function getQuestion() {
+// 	console.log("We are here!");
+
+// 	var h1El = document.getElementById("question");
+// 	h1El.textContent = questions[0].question;
+// 	var divChoice = document.getElementById("divChoice");
+
+// 	divChoice.innerHTML = "";
+
+// 	var test = document.getElementById("testing");
+
+//divChoice.textContent = questions[0].choices;
+
+// for (let i = 0; i < questions[0].choices.length; i++) {
+// 	var button = document.createElement('button');
+// 	button.textContent = questions[0].choices[i]
+// 	divChoice.append(button);
+
+// 	button.onclick = checkAnswer
+
+// var divEl = document.createElement("div");
+// divEl.setAttribute("class", "form-check");
+// var input = document.createElement("input");
+// input.setAttribute("class", "form-check-input");
+// input.setAttribute("type", "radio");
+// input.setAttribute("value", questions[0].choices[i]);
+
+// var label = document.createElement("label");
+// label.setAttribute("class", "form-check-label");
+// label.textContent = questions[0].choices[i];
+// input.append(label);
+// divEl.append(input);
+
+// console.log("divEl", divEl);
+// divChoice.append(divEl);
+// test.append(divEl);
+// }
+// }
+
+// -------------------------------------------------------- //
+// getQuestion();
+
+// function sendMessage() {
+// 	timeEl.textContent = " ";
+
+// 	let imgEl = document.createElement("img");
+
+// 	imgEl.setAttribute("src", "images/image_1.jpg");
+// 	mainEl.appendChild(imgEl);
+// }
+
+// ----------------------------------------------------------//
+// let questions = [
+// 	{
+// 		question: "What is a HTML header used for?",
+// 		choices: [
+// 			"Titles or subtitles that you want to display on a webpage.",
+// 			"Text at the bottom of the page.",
+// 			"Styles your HTML",
+// 			"All the above",
+// 		],
+// 		correctAnswer: "Titles or subtitles that you want to display on a webpage.",
+// 	},
+// 	{
+// 		question: "What is the use for CSS?",
+// 		choices: [
+// 			"To clean your page.",
+// 			"To open your website on a browser.",
+// 			"Lets you style your website.",
+// 			"Lets your website be responsive.",
+// 		],
+// 		correctAnswer: "Lets you style your website.",
+// 	},
+// 	{
+// 		question: "What is a HTML header used for?",
+// 		choices: [
+// 			"Titles or subtitles that you want to display on a webpage.",
+// 			"Text at the bottom of the page.",
+// 			"Styles your HTML",
+// 			"All the above",
+// 		],
+// 		correctAnswer: "Titles or subtitles that you want to display on a webpage.",
+// 	},
+// ];
