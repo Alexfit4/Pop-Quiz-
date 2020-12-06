4379ed002fd418ae14a0044bdaad1781da7114b6
Alex Ashtiany <alex.ashtiany@gmail.com>
2020-12-05 21:22:25 -0500

    working on form page

--- asset/script.js
+++ asset/script.js
@@ -1,3 +1,5 @@
+// Variables //
+
 let startQuiz = document.querySelector(".startBtn");
 let instructionContainer = document.querySelector("#instructionContainer");
 let questionContainer = document.querySelector("#questionContainer");
@@ -7,50 +9,59 @@ let timerID;
 let score = 0;
 let currentQuestion = -1;
 
+// My array with the questions and answers //
 const questions = [
 	{
-		question: "What is a HTML header used for?",
+		question: "1) What is a HTML header used for?",
 		choices: [
-			"Titles or subtitles that you want to display on a webpage.",
-			"Text at the bottom of the page.",
-			"Styles your HTML",
-			"All the above",
+			" Titles or subtitles that you want to display on a webpage.",
+			" Text at the bottom of the page.",
+			" Styles your HTML",
+			" All the above",
 		],
 		correctAnswer: 0,
 	},
 	{
-		question: "What is the use for CSS?",
+		question: "2) What is the use for CSS?",
 		choices: [
-			"To clean your page.",
-			"To open your website on a browser.",
-			"Lets you style your website.",
-			"Lets your website be responsive.",
-		],
-		correctAnswer: 2,
-	},
-	{
-		question: "Which of the following is correct about features of JavaScript?",
-		choices: [
-			"JavaScript variable names must begin with a letter or the underscore character.",
-			"JavaScript variable names are case sensitive.",
-			"Both of the above.",
-			"None of the above.",
+			" To clean your page.",
+			" To open your website on a browser.",
+			" Lets you style your website.",
+			" Lets your website be responsive.",
 		],
 		correctAnswer: 2,
 	},
 	{
 		question:
-			"Which built-in method removes the last element from an array and returns that element?",
-		choices: ["last()", "get()", "pop()", "None of the above"],
+			"3) Which of the following is correct about features of JavaScript?",
+		choices: [
+			" JavaScript variable names must begin with a letter or the underscore character.",
+			" JavaScript variable names are case sensitive.",
+			" Both of the above.",
+			" None of the above.",
+		],
 		correctAnswer: 2,
 	},
 	{
 		question:
-			"Which built-in method returns the string representation of the number's value?",
-		choices: ["toValue()", "toNumber()", "toString()", "None of the above."],
+			"4) Which built-in method removes the last element from an array and returns that element?",
+		choices: [" last()", " get()", " pop()", " None of the above"],
+		correctAnswer: 2,
+	},
+	{
+		question:
+			"5) Which built-in method returns the string representation of the number's value?",
+		choices: [
+			" toValue()",
+			" toNumber()",
+			" toString()",
+			" None of the above.",
+		],
 		correctAnswer: 2,
 	},
 ];
+
+// When user clicks "CLICK HERE TO START" quiz timer starts and first line of questions begin. //
 startQuiz.addEventListener("click", function () {
 	instructionContainer.style.display = "none";
 	questionContainer.style.display = "block";
@@ -58,6 +69,7 @@ startQuiz.addEventListener("click", function () {
 	startTimer();
 });
 
+// function to update time every second, and when time is up it alerts the user the quiz is over and show their results. //
 function updateTime() {
 	remainingTime--;
 	document.querySelector(".time").innerHTML = "Time: " + remainingTime;
@@ -69,14 +81,17 @@ function updateTime() {
 	}
 }
 
+// Function to start the timer. //
 function startTimer() {
 	timerID = setInterval(updateTime, 1000);
 }
 
+// funtion to stop the timer
 function stopTimer() {
 	clearInterval(timerID);
 }
 
+// function that checks the answer to each question and depending if the user got the answer right or wrong they are alerted. If the user is wrong 10 seconds are removed from there remaining time. Then after they submit the question the next line of questions show up. //
 function checkAnswer() {
 	let answers = document.getElementsByName("question");
 	let selectedAnswer;
@@ -88,10 +103,11 @@ function checkAnswer() {
 	}
 
 	if (selectedAnswer == questions[currentQuestion].correctAnswer) {
-		alert("correct");
+		alert("Correct!");
 		score++;
 	} else {
-		alert("wrong");
+		alert("Wrong");
+		remainingTime = remainingTime - 10;
 	}
 
 	showNextQuestion();
@@ -101,6 +117,7 @@ questionContainer
 	.querySelector("button")
 	.addEventListener("click", checkAnswer);
 
+// Function showing the next question and when there arent anymore questions the timer stops and the results are shown. //
 function showNextQuestion() {
 	let question = questions[(currentQuestion += 1)];
 	let answers = [];
@@ -132,9 +149,12 @@ function showNextQuestion() {
 	);
 }
 
+// Function showing the final score the user recieved. //
+
 function showResults() {
 	questionContainer.style.display = "none";
 	resultContainer.style.display = "block";
 
-	document.querySelector("#totalScore").innerHTML = "You scored " + score;
+	document.querySelector("#totalScore").innerHTML =
+		"You scored " + score + " out of 5 points";
 }
